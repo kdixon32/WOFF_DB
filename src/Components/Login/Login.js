f657b0791cd939ab886ae26158c45e2db5e660b2
Kielan <kdixon32@student.gsu.edu>
2022-04-17 01:36:28 -0400

    troubleshooting

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,62 +1,24 @@
-import React, { useState } from 'react';
 import { Form, Button, Card, Container } from 'react-bootstrap';
-import { Link, Navigate } from 'react-router-dom';
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { auth } from './Firebase';
-import Signup from './Signup';
-import Members from '../Members';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    useNavigate,
-} from 'react-router-dom';
-export default function Login() {
+import React, { Component } from 'react';
 
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
-    const [user, setUser] = useState({});
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-    });
-
-    const handleSubmit = async () => {
-        try {
-            const user = await signInWithEmailAndPassword(
-                auth,
-                loginEmail,
-                loginPassword
-            );
-            console.log(user);
-
-
-        } catch (error) {
-            console.log(error.message);
-        }
-        Navigate("/go");
-    };
-
-
+export default class Login extends Component {
+    render() {
     return (
         <div >
             <Container className="d-flex align-items-center justify-content-center" >
                 <Card>
                     <Card.Body>
                         <h2 className="text-center mb-4">Log In</h2>
-                        <Form onSubmit={handleSubmit}>
+                        <Form>
                             <Form.Group id="email">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" onChange={(event) => {
-                                    setLoginEmail(event.target.value);
-                                }} required />
+                                <Form.Control type="email"  required />
                             </Form.Group>
                             <Form.Group id="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" onChange={(event) => {
-                                    setLoginPassword(event.target.value);
-                                }} required />
+                                <Form.Control type="password" required />
                             </Form.Group>
-                            <Button className="w-100" onClick="{handlesubmit}" type="submit">
+                            <Button className="w-100" type="submit">
                                 Log In!
                             </Button>
                         </Form>
@@ -64,12 +26,8 @@ export default function Login() {
                 </Card>
             </Container>
             <div className="w-100 text-center mt-2" style={{ maxWidth: "400px" }}>
-                    <Routes>
-                        <Route path="/go" element={<Navigate to="Members" />}></Route>
-                        <Route path="/signup" element={<Signup />}></Route>
-                    </Routes>
             </div>
         </div >
     )
-
+    }
 }
--- src/Components/Login/Signup.js
+++ src/Components/Login/Signup.js
@@ -1,61 +1,24 @@
-import React, { useState } from 'react';
 import { Form, Button, Card, Container } from 'react-bootstrap';
-import { Link, Navigate } from 'react-router-dom';
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { auth } from './Firebase';
-import Login from './Login';
-import Members from '../Members';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-
-} from 'react-router-dom';
-export default function Signup() {
-
-    const [signupEmail, setRegisterEmail] = useState("");
-    const [signupPassword, setRegisterPassword] = useState("");
-
-    const [user, setUser] = useState({});
-
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-    });
-    const handleSubmit = async () => {
-        try {
-            const user = await createUserWithEmailAndPassword(
-                auth,
-                signupEmail,
-                signupPassword
-            );
-            console.log(user);
-            const ProtectedRoute = () => {
-                return <Navigate to="/members" />
-            }
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+import React, { Component } from 'react';
 
 
+export default class Signup extends Component {
+    render() {
+   
     return (
         <div >
             <Container className="d-flex align-items-center justify-content-center" >
                 <Card>
                     <Card.Body>
                         <h2 className="text-center mb-4">Sign Up</h2>
-                        <Form onSubmit={handleSubmit}>
+                        <Form>
                             <Form.Group id="email">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" onChange={(event) => {
-                                    signupEmail(event.target.value);
-                                }} required />
+                                <Form.Control type="email" required />
                             </Form.Group>
                             <Form.Group id="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" onChange={(event) => {
-                                    signupPassword(event.target.value);
-                                }} required />
+                                <Form.Control type="password" required />
                             </Form.Group>
                             <Button className="w-100" type="submit">
                                 Sign Up!
@@ -65,11 +28,8 @@ export default function Signup() {
                 </Card>
             </Container>
             <div className="w-100 text-center mt-2" style={{ maxWidth: "400px" }}>
-                <Routes>
-                    <Route path="/login" element={<Login />}></Route>
-                </Routes>
             </div>
         </div>
     )
-
+    }
 }
