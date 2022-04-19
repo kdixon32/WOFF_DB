7ed397e25c6c4bc97e1791b7b84733d592c29306
Kielan <kdixon32@student.gsu.edu>
2022-04-19 01:15:27 -0400

    login message

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,31 +3,31 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
     render() {
-    return (
-        <div >
-            <Container className="d-flex align-items-center justify-content-center" >
-                <Card>
-                    <Card.Body>
-                        <h2 className="text-center mb-4">Log In</h2>
-                        <Form>
-                            <Form.Group id="email">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email"  required />
-                            </Form.Group>
-                            <Form.Group id="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" required />
-                            </Form.Group>
-                            <Button className="w-100" type="submit">
-                                Log In!
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </Container>
-            <div className="w-100 text-center mt-2" style={{ maxWidth: "400px" }}>
-            </div>
-        </div >
-    )
+        return (
+            <div >
+                <Container className="d-flex align-items-center justify-content-center" >
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Log In</h2>
+                            <Form>
+                                <Form.Group id="email">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" required />
+                                </Form.Group>
+                                <Form.Group id="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" required />
+                                </Form.Group>
+                                <Button className="w-100" type="submit">
+                                    Log In!
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Container>
+                <div className="center-2">I'm broken :(
+                </div>
+            </div >
+        )
     }
 }
